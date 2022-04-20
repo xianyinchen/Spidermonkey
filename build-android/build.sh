e96d7ed6c761eef65e8da453755ed99c52026421
chenxianyin <420373550@qq.com>
2022-04-20 16:15:24 +0800

    update.

--- build-android/build.sh
+++ build-android/build.sh
@@ -1,3 +1,5 @@
+export NDK_ROOT=/Users/chenxianyin/Desktop/android-ndk-r14b
+export ANDROID_SDK_HOME=/Users/chenxianyin/Library/Android/sdk/
 
 # options
 develop=
@@ -58,7 +60,7 @@ rm -f ./config.cache
 python ../configure.py \
             --enable-project=js \
             --with-android-ndk=$NDK_ROOT \
-            --with-android-sdk=$HOME/bin/android-sdk \
+            --with-android-sdk=$ANDROID_SDK_HOME \
             --with-android-version=${ANDROID_VERSION} \
             --with-android-gnu-compiler-version=${GCC_VERSION} \
             --with-arch=${CPU_ARCH} \
@@ -102,28 +104,6 @@ fi
 
 }
 
-# Build with armv6
-TOOLS_ARCH=arm-linux-androideabi
-TARGET_NAME=arm-linux-androideabi
-CPU_ARCH=armv6
-RELEASE_ARCH_DIR=armeabi
-GCC_VERSION=4.9
-TOOLNAME_PREFIX=arm-linux-androideabi
-ANDROID_VERSION=9
-EXTRA_ARGS=--disable-jemalloc
-build_with_arch
-
-# Build with armv7
-TOOLS_ARCH=arm-linux-androideabi
-TARGET_NAME=arm-linux-androideabi
-CPU_ARCH=armv7-a
-RELEASE_ARCH_DIR=armeabi-v7a
-GCC_VERSION=4.9
-TOOLNAME_PREFIX=arm-linux-androideabi
-ANDROID_VERSION=9
-EXTRA_ARGS=--disable-jemalloc
-build_with_arch
-
 # Build with arm64
 TOOLS_ARCH=aarch64-linux-android
 TARGET_NAME=aarch64-linux-android
@@ -132,16 +112,5 @@ RELEASE_ARCH_DIR=arm64-v8a
 GCC_VERSION=4.9
 TOOLNAME_PREFIX=aarch64-linux-android
 ANDROID_VERSION=21
-EXTRA_ARGS=--disable-jemalloc
-build_with_arch
-
-# Build with x86
-TOOLS_ARCH=x86
-TARGET_NAME=i686-linux-android
-CPU_ARCH=i686
-RELEASE_ARCH_DIR=x86
-GCC_VERSION=4.9
-TOOLNAME_PREFIX=i686-linux-android
-ANDROID_VERSION=9
 EXTRA_ARGS=--disable-jemalloc
 build_with_arch
---
+++ build-ndk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# brew install autoconf@2.13
+
+# brew install zlib
+# change zip version on build
+
